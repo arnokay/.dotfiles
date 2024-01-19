32b6b017799db78b1792218f864c911318a00248
arnokay <arnokay.work@gmail.com>
2024-01-19 06:25:51 +0200

    feat(packages-golang): update go version

--- packages/golang.sh
+++ packages/golang.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# Define the Go version
-GO_VERSION="1.17.6"
+GO_VERSION="1.21.6"
 ARCH="linux-amd64"
 
 echo "Removing existing Go installation..."
-# Remove existing Go installation
 sudo rm -rf /usr/local/go
 
 echo "Installing Go..."
 
-# Download Go
 curl -LO https://golang.org/dl/go$GO_VERSION.$ARCH.tar.gz
 
-# Extract and install Go
 sudo tar -C /usr/local -xvf go$GO_VERSION.$ARCH.tar.gz
 
-# Check if installation was successful
 if command -v go &> /dev/null; then
     echo "Go $GO_VERSION installed successfully."
 else
@@ -24,7 +19,6 @@ else
     exit 1
 fi
 
-# Clean up downloaded archive
 rm go$GO_VERSION.$ARCH.tar.gz
 
 echo "Finished installing Go."
